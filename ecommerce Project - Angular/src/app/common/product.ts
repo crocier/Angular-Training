df5bfefb0a32dc75bf9ade86d3c204565b678867
ajaicrocier <ajaicrocier@gmail.com>
2021-09-20 23:08:09 +0530

    Ecommerce Project using Angular

---
+++ ecommerce Project - Angular/src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    ssku : string;
+    name : string;
+    description : string;
+    unitPrice : number;
+    imageUrl : string;
+    active : boolean;
+    unitsInStock : number;
+    dateCreated : Date;
+    lastUpdated : Date;
+}
